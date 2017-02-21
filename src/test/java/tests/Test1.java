1cd19ec1af7ff4de651494180227c00d1e4ceb2e
Gladson Antony <gladsonantony91@gmail.com>
2017-02-21 22:08:31 +0530

    POI Changed

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import controllers.ExcelDataProvider;
 import pageObjects.initializePageObjects.PageFactoryInitializer;
 import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Parameter;
 import ru.yandex.qatools.allure.annotations.TestCaseId;
 
 public class Test1 extends PageFactoryInitializer
@@ -12,7 +13,7 @@ public class Test1 extends PageFactoryInitializer
 	@TestCaseId("TC_GMail_001")
 	@Description("To verify the working of GMail link from Google Home Page using JavaScript Executor")
 	@Test(dataProvider="excelSheetNameAsMethodName", dataProviderClass=ExcelDataProvider.class)
-	public void testGoogle(String TestCaseID, String UserName, String Password) throws Exception
+	public void testGoogle(@Parameter("TestCaseID")String TestCaseID, @Parameter("UserName")String UserName,@Parameter("Password") String Password) throws Exception
 	{
 		System.out.println("TestCase ID: "+TestCaseID);
 		System.out.println("UserName: "+UserName);
