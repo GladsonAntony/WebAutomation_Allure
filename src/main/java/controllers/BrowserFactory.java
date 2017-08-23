0bdf4c86bdb941a89353f8d8d3a05d50f7d2b89b
Gladson Antony <GladsonAntony91@gmail.com>
2017-08-23 19:41:43 +0530

    Updated Selenium to 3.5.2, Added Chrome_Headless Browser Support

--- src/main/java/controllers/BrowserFactory.java
+++ src/main/java/controllers/BrowserFactory.java
@@ -6,11 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.opera.OperaOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -22,6 +19,9 @@ import org.testng.annotations.BeforeClass;
 
 public class BrowserFactory extends TestHelper
 {
+	static DesiredCapabilities capabilities;
+
+	@SuppressWarnings("deprecation")
 	@BeforeClass(enabled = true)
 	public void StartBrowser() 
 	{
@@ -32,21 +32,9 @@ public class BrowserFactory extends TestHelper
 			if (OSName.contains("Windows")) 
 			{
 				System.setProperty("webdriver.gecko.driver", WindowsDrivers + "geckodriver.exe");
-
-				FirefoxProfile firefoxProfile = new FirefoxProfile();
-				firefoxProfile.setPreference("browser.download.manager.showWhenStarting", false);
-				firefoxProfile.setPreference("browser.download.folderList", 1);
-				firefoxProfile.setPreference("browser.helperApps.neverAsk.saveToDisk",
-						"application/zip;application/octet-stream;application/x-zip;application/x-zip-compressed");
-				firefoxProfile.setPreference("browser.startup.homepage_override.mstone", "about:blank");
-				firefoxProfile.setPreference("browser.startup.homepage", "about:blank");
-				firefoxProfile.setPreference("startup.homepage_welcome_url", "about:blank");
-				firefoxProfile.setPreference("startup.homepage_welcome_url.additional", "about:blank");
-				firefoxProfile.setPreference("browser.usedOnWindows10.introURL", "about:blank");
-				driver = new FirefoxDriver(firefoxProfile);
+				driver = new FirefoxDriver();
 				driver.manage().deleteAllCookies();
 				driver.manage().window().maximize();
-				driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 				driver.get(WebsiteURL);
 			} 
 			else if (OSName.contains("Ubuntu") || OSName.contains("Linux"))
@@ -88,6 +76,43 @@ public class BrowserFactory extends TestHelper
 			}
 		}
 
+		else if (BrowserType.toLowerCase().equalsIgnoreCase("chrome_headless")) 
+		{
+			if (OSName.contains("Windows")) 
+			{
+				System.setProperty("webdriver.chrome.driver", WindowsDrivers + "chromedriver.exe");
+				ChromeOptions chromeOptions = new ChromeOptions();
+				chromeOptions.addArguments("--headless");  
+				chromeOptions.addArguments("--disable-gpu");  
+				driver = new ChromeDriver(chromeOptions);
+				driver.manage().window().maximize();
+				driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+				driver.get(WebsiteURL);
+			} 
+			else if (OSName.contains("Ubuntu") || OSName.contains("Linux")) 
+			{
+				System.setProperty("webdriver.chrome.driver", LinuxDrivers + "chromedriver");
+				ChromeOptions chromeOptions = new ChromeOptions();
+				chromeOptions.addArguments("--headless");  
+				chromeOptions.addArguments("--disable-gpu");  
+				driver = new ChromeDriver(chromeOptions);
+				driver.manage().window().maximize();
+				driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+				driver.get(WebsiteURL);
+			} 
+			else if (OSName.contains("Mac"))
+			{
+				System.setProperty("webdriver.chrome.driver", MacDrivers + "chromedriver");
+				ChromeOptions chromeOptions = new ChromeOptions();
+				chromeOptions.addArguments("--headless");  
+				chromeOptions.addArguments("--disable-gpu");  
+				driver = new ChromeDriver(chromeOptions);
+				driver.manage().window().maximize();
+				driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+				driver.get(WebsiteURL);
+			}
+		}
+
 		else if (BrowserType.toLowerCase().equalsIgnoreCase("ie"))
 		{
 			if (OSName.contains("Windows")) 
@@ -108,17 +133,15 @@ public class BrowserFactory extends TestHelper
 		{
 			if (OSName.contains("Windows")) 
 			{
-				System.setProperty("webdriver.opera.driver", WindowsDrivers + "operadriver.exe");
-				OperaOptions options = new OperaOptions();
-				//ChromeOptions options = new ChromeOptions();
-				options.setBinary("C:\\Program Files (x86)\\Opera\\launcher.exe");        
-
-				DesiredCapabilities capabilities = new DesiredCapabilities();
-				capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-				OperaDriver browser = new OperaDriver(capabilities);
-				browser.manage().window().maximize();
-				browser.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-				browser.get(WebsiteURL);
+				System.setProperty("webdriver.chrome.driver", WindowsDrivers + "operadriver.exe");
+				capabilities = DesiredCapabilities.opera();
+				ChromeOptions optionsOpera = new ChromeOptions();
+				optionsOpera.setBinary("C:/Program Files/Opera/launcher.exe");
+				capabilities.setCapability(ChromeOptions.CAPABILITY, optionsOpera);
+				driver = new ChromeDriver(capabilities);
+				driver.manage().window().maximize();
+				driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+				driver.get(WebsiteURL);
 			} 
 			else if (OSName.contains("Ubuntu") || OSName.contains("Linux")) 
 			{
@@ -163,13 +186,12 @@ public class BrowserFactory extends TestHelper
 			System.out.println("Please Provide a Valid Browser");
 		}
 	}
-	
-	
+
+
 	@AfterClass(enabled=true, alwaysRun = true)
 	public void tearDown() throws Exception 
 	{
 		Thread.sleep(5000);
 		driver.quit();
 	}
-	
 }
